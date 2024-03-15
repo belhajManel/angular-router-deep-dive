6d219496e7f0c1561fcbfc969233f3c5cdb9ea25
Manel BELHADJ <belhaj343@gmail.com>
2024-03-15 12:11:07 +0100

    added navigation functionality

--- src/app/courses/lesson/lesson-detail.component.ts
+++ src/app/courses/lesson/lesson-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { LessonDetail } from "../model/lesson-detail";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
@@ -12,11 +12,24 @@ import { map } from "rxjs/operators";
 export class LessonDetailComponent implements OnInit {
   lesson$: Observable<LessonDetail>;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private router: Router) {
     console.log("Created LessonDetailComponent...");
   }
 
   ngOnInit() {
-    this.lesson$ = this.route.snapshot.data["lesson"];
+    this.lesson$ = this.route.data.pipe(map((data) => data["lesson"]));
+    // this.lesson$ = this.route.snapshot.data["lesson"];
+  }
+
+  previous(lesson: LessonDetail) {
+    this.router.navigate(["lessons", lesson.seqNo - 1], {
+      relativeTo: this.route.parent,
+    });
+  }
+
+  next(lesson: LessonDetail) {
+    this.router.navigate(["lessons", lesson.seqNo + 1], {
+      relativeTo: this.route.parent,
+    });
   }
 }
